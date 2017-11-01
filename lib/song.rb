75393ed809fc26d3e59e747c48db35d6222a7908
Araba Taylor <araba.taylor20@gmail.com>
2017-11-01 00:58:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ class Song
   def self.find_or_create_by_name(song_name)
     #binding.pry
     if song = self.find_by_name(song_name)
-      self.find_by_name(song_name)
+      song
     else
       self.create_by_name(song_name)
     end
